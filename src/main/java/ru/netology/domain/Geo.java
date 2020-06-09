079e36f5fe058c9ea5c497f17338f117a784e379
Ulyana Ermoshina <uermoshina@inbox.ru>
2020-06-09 17:27:20 +0300

    v1

---
+++ src/main/java/ru/netology/domain/Geo.java
@@ -0,0 +1,25 @@
+package ru.netology.domain;
+
+public class Geo {
+
+    private String type;
+    private String Coordinates;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getCoordinates() {
+        return Coordinates;
+    }
+
+    public void setCoordinates(String coordinates) {
+        Coordinates = coordinates;
+    }
+
+
+}
---
+++ src/main/java/ru/netology/domain/Post.java
@@ -0,0 +1,148 @@
+package ru.netology.domain;
+
+public class Post {
+private int id;
+private int ownerId;
+private int fromId;
+private int createdBy;
+private int date;
+private int replyOwnerId;
+private int replyPostId;
+private int copyright;
+private int signerId;
+private int postponedId;
+private CommentsInfo commentsInfo;
+private LikesInfo likesInfo;
+private RepostsInfo repostsInfo;
+private viewsInfo viewsInfo;
+private PostSource postSource;
+private Geo geo;
+
+    public CommentsInfo getCommentsInfo() {
+        return commentsInfo;
+    }
+
+    public void setCommentsInfo(CommentsInfo commentsInfo) {
+        this.commentsInfo = commentsInfo;
+    }
+
+    public LikesInfo getLikesInfo() {
+        return likesInfo;
+    }
+
+    public void setLikesInfo(LikesInfo likesInfo) {
+        this.likesInfo = likesInfo;
+    }
+
+    public RepostsInfo getRepostsInfo() {
+        return repostsInfo;
+    }
+
+    public void setRepostsInfo(RepostsInfo repostsInfo) {
+        this.repostsInfo = repostsInfo;
+    }
+
+    public ru.netology.domain.viewsInfo getViewsInfo() {
+        return viewsInfo;
+    }
+
+    public void setViewsInfo(ru.netology.domain.viewsInfo viewsInfo) {
+        this.viewsInfo = viewsInfo;
+    }
+
+    public PostSource getPostSource() {
+        return postSource;
+    }
+
+    public void setPostSource(PostSource postSource) {
+        this.postSource = postSource;
+    }
+
+    public Geo getGeo() {
+        return geo;
+    }
+
+    public void setGeo(Geo geo) {
+        this.geo = geo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getOwnerId() {
+        return ownerId;
+    }
+
+    public void setOwnerId(int ownerId) {
+        this.ownerId = ownerId;
+    }
+
+    public int getFromId() {
+        return fromId;
+    }
+
+    public void setFromId(int fromId) {
+        this.fromId = fromId;
+    }
+
+    public int getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(int createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public int getDate() {
+        return date;
+    }
+
+    public void setDate(int date) {
+        this.date = date;
+    }
+
+    public int getReplyOwnerId() {
+        return replyOwnerId;
+    }
+
+    public void setReplyOwnerId(int replyOwnerId) {
+        this.replyOwnerId = replyOwnerId;
+    }
+
+    public int getReplyPostId() {
+        return replyPostId;
+    }
+
+    public void setReplyPostId(int replyPostId) {
+        this.replyPostId = replyPostId;
+    }
+
+    public int getCopyright() {
+        return copyright;
+    }
+
+    public void setCopyright(int copyright) {
+        this.copyright = copyright;
+    }
+
+    public int getSignerId() {
+        return signerId;
+    }
+
+    public void setSignerId(int signerId) {
+        this.signerId = signerId;
+    }
+
+    public int getPostponedId() {
+        return postponedId;
+    }
+
+    public void setPostponedId(int postponedId) {
+        this.postponedId = postponedId;
+    }
+}
